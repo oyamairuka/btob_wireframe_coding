781eb6c6d449911a946f92a24b384a6143d5117d
eiji-shimizu <eijishimizu9@gmail.com>
2022-12-10 16:18:14 +0900

    トップページワイヤーフレーム「特徴」のあたりまで

--- js/initializeHome.js
+++ js/initializeHome.js
@@ -3,7 +3,13 @@ function initialize() {
     document.getElementById('catchphraseEx').innerHTML = catchphraseEx;
     document.getElementById('contactButtonTop').textContent = contactButtonCaption;
 
-    document.getElementById('home_feature').innerText = "home_feature";
+    document.getElementById('feature_name').textContent = FEATURE.name;
+    document.getElementById('feature_title').textContent = FEATURE.title;
+    document.getElementById('feature_caption1').innerHTML = FEATURE.caption1;
+    document.getElementById('feature_caption2').innerHTML = FEATURE.caption2;
+    document.getElementById('feature_caption3').innerHTML = FEATURE.caption3;
+    document.getElementById('featureButtonTop').textContent = FEATURE.buttonCaption;
+
     document.getElementById('home_function').innerText = "home_function";
 }
 
