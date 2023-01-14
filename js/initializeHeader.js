01648837dc1f7a62530a65144a00dabf72b69cf1
eiji-shimizu <eijishimizu9@gmail.com>
2023-01-14 15:34:45 +0900

    SPレイアウト時メニュー項目を追加

--- js/initializeHeader.js
+++ js/initializeHeader.js
@@ -9,6 +9,26 @@ function initialize() {
         link.classList.add('header_menu_link');
         e.appendChild(link);
         document.getElementById('header_menu').appendChild(e);
+
+        // スマホレイアウト時のメニュー
+        var div = document.createElement('div');
+        div.classList.add('menu_container_menu_text');
+        var a = document.createElement('a');
+        a.href = element.path;
+        a.text = element.name;
+        div.appendChild(a);
+        document.getElementById('sp_menu').appendChild(div);
+    });
+
+    // スマホレイアウト時のメニューには追加
+    footerMenu.forEach(element => {
+        var div = document.createElement('div');
+        div.classList.add('menu_container_menu_text');
+        var a = document.createElement('a');
+        a.href = element.path;
+        a.text = element.name;
+        div.appendChild(a);
+        document.getElementById('sp_menu').appendChild(div);
     });
 
     document.getElementById('header_phone_number').innerHTML = phoneNumber;
